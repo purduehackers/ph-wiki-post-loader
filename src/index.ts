771df5b77e41b8a06a71effe4fab1a7b27e1bfb3
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-11-05 11:04:59 -0500

    add rate limit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ console.log("app started");
 import "dotenv/config";
 
 import { Octokit } from "octokit";
-
+import { OctokitOptions } from "@octokit/core/dist-types/types.d";
 import { connectDB, disconnectDB } from "./db/db";
 import { Path } from "./db/model/Path";
 import { Post } from "./db/model/Post";
@@ -13,7 +13,25 @@ import saveToPath from "./helper/saveToPath";
 import RepoStructNode from "./type/RepoStructNode";
 import TreeNode from "./type/TreeNode";
 
-const octokit = new Octokit({});
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+  throttle: {
+    onRateLimit: (retryAfter, options: OctokitOptions, octokit, retryCount) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      );
+      if (retryCount < 1) {
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options: OctokitOptions, octokit) => {
+      octokit.log.warn(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+      );
+    },
+  },
+});
 
 const load = async () => {
   const newestCommit = await fetchNewestCommit(octokit);
@@ -32,14 +50,10 @@ const load = async () => {
     children: [],
   };
   await buildTree(root, repoStructRoot, octokit);
-  // console.log("root", repoStructRoot);
-
   await connectDB();
-
   await Path.collection.drop();
   await Post.collection.drop();
   await saveToPath(repoStructRoot, octokit);
-
   await disconnectDB();
 };
 
