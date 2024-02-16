4f3fcc963325af0d2f4ec5f01afbe154a1421833
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-02-16 10:49:57 -0500

    set up dev env

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,25 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(process.env.MONGODB_URI || '')
-    .then(() => {
-      console.log('Connected to MongoDB')
-    })
-    .catch(() => {
-      console.log("Couldn't connect to MongoDB")
-    })
+  if (process.env.NODE_ENV === 'production') {
+    await mongoose
+      .connect(process.env.MONGODB_URI || '')
+      .then(() => {
+        console.log('Connected to MongoDB')
+      })
+      .catch(() => {
+        console.log("Couldn't connect to MongoDB")
+      })
+  } else {
+    await mongoose
+      .connect(`mongodb://127.0.0.1:27017/${process.env.DEV_DB_NAME}`)
+      .then(() => {
+        console.log('Connected to MongoDB')
+      })
+      .catch(() => {
+        console.log("Couldn't connect to MongoDB")
+      })
+  }
 }
 
 export const disconnectDB = async () => {
